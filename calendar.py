dee1221d343ad76a5a5f81126e6f1d91c183c333
DevilanD <68126654+dAYOShACKER505@users.noreply.github.com>
2020-09-06 10:38:15 +0000

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,170 @@
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 1    # month
+
+
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+print(".........................")
+print(calendar.month (yy, mm ))
+print(".........................")
+
+# display the calendarProgram to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 2    # month
+
+
+
+
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+
+
+print(".........................")
+print(calendar.month (yy, mm ))
+print(".........................")
+
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 3    # month
+
+
+
+
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 4    # month
+
+#display the calendar 
+print(".........................")
+print(calendar .month (yy, mm,))
+print(".........................")
+
+
+
+
+
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm = 5  #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm = 6  #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm =  7 #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm =  8 #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm = 9  #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm = 11  #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+
+#importing calendar module
+import calendar 
+yy = 2020 #year
+mm = 12  #month
+
+
+
+
+# display the calendar
+print(".........................")
+print(calendar.month (yy, mm  ,))
+print(".........................")
+print(".........................")
+
+
